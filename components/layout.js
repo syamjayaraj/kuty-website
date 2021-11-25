befddf5ac76edc97974cb63e7a67900729a80028
Syamlal <syamjayaraj@gmail.com>
2021-11-25 16:18:55 +0530

    minor

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,6 @@ import Meta from "../components/meta";
 export default function Layout({ preview, children }) {
   return (
     <>
-      <Meta />
       <div className="min-h-screen">
         {/* <Alert preview={preview} /> */}
         <main>{children}</main>
